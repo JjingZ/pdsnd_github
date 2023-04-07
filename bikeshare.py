61ad6ccdd8034fff8209983e393f47c0e608d835
alsyz <shengyunzhe@hotmail.com>
2023-04-07 16:10:58 -0500

    update comment in bikeshre.py

--- bikeshare.py
+++ bikeshare.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import pprint
 
-# test update for GitHub project
+# test2 update for GitHub project
 
 CITY_DATA = {'chicago': 'chicago.csv',
              'new york city': 'new_york_city.csv',
